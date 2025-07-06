9d2dfd814124f360ac23edc1b6793707261e2e2e
Steven Kay <steven.m.kay@live.com>
2025-07-06 10:18:18 -0700

    Create contentItems.js

---
+++ js/contentItems.js
@@ -0,0 +1,54 @@
+const contentItems = [
+    {
+      section: 'Templates',
+      title: 'Excel Budget Spreadsheet',
+      description: 'A downloadable spreadsheet to manage your personal or professional budget.',
+      thumbnail: 'https://img.icons8.com/color/96/000000/microsoft-excel-2019--v1.png',
+      link: 'https://docs.google.com/spreadsheets/d/18KX2DZyZGsxFXZtM4rtybY2rhLZQMWWdPiPau-Cpyuo/export?format=xlsx'
+    },
+
+{
+  section: 'Practice Documents',
+  title: 'Practice Workbook - Dynamic Text Filter',
+  description: 'A downloadable spreadsheet to practice the dynamic text filter capability.',
+  thumbnail: 'https://img.icons8.com/color/96/000000/microsoft-excel-2019--v1.png',
+  link: 'https://docs.google.com/spreadsheets/d/1taPzeomXBu-O1G-FmmVhlBmBYNeqMTu5/export?format=xlsx',
+  related: {
+    text: 'Video explaining function',
+    url: 'https://youtu.be/sdall4s4_RY'
+  }
+},
+
+
+    {
+      section: 'Videos',
+      title: 'Resume Tips',
+      description: 'Step-by-step resume writing strategies.',
+      thumbnail: 'https://img.icons8.com/ios/100/000000/video.png',
+      link: '#videos'
+    },
+{
+  section: 'Templates',
+  program: {
+    name: 'PowerPoint',
+    image: 'https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/43b55f504d147fe575506b0ad439d4b363b3613c/PowerPoint%20Image.png'
+  },
+  title: 'Modern Resume Template',
+  description: 'Fillable modern resume template with sections including skills, education, and more.',
+  thumbnail: 'https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/43b55f504d147fe575506b0ad439d4b363b3613c/Thumbnail%20-%20Modern%20Resume%20Template.png',
+  link: 'https://docs.google.com/presentation/d/1q56RDcpNKYnM69j6ElT9LfV04faAcRDu/export?format=pptx'
+},
+
+
+{
+  section: 'Templates',
+  program: {
+    name: 'PowerPoint',
+    image: 'https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/43b55f504d147fe575506b0ad439d4b363b3613c/PowerPoint%20Image.png'
+  },
+  title: 'Modern Resume Template with Photo',
+  description: 'A PowerPoint resume layout that includes a space for your photo and personalized details.',
+  thumbnail: 'https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/6bcca2d9951dd2fa2acfdd7037e3da74f59f025f/Thumbnail%20-%20Modern%20Resume%20Template%20with%20Photo.png',
+  link: 'https://docs.google.com/presentation/d/1hnGjNDEAXbPqIY6b_9niedbhqE8wJFdG/export?format=pptx'
+}
+  ];
