b58c13be03e4da532fa5b1cd40c3c2a981c32df5
Steven Kay <steven.m.kay@live.com>
2025-07-06 12:06:58 -0700

    Update contentItems.js

--- js/contentItems.js
+++ js/contentItems.js
@@ -1,14 +1,28 @@
 const contentItems = [
     {
-      section: 'Templates',
-      title: 'Excel Budget Spreadsheet',
-      description: 'A downloadable spreadsheet to manage your personal or professional budget.',
-      thumbnail: 'https://img.icons8.com/color/96/000000/microsoft-excel-2019--v1.png',
-      link: 'https://docs.google.com/spreadsheets/d/18KX2DZyZGsxFXZtM4rtybY2rhLZQMWWdPiPau-Cpyuo/export?format=xlsx'
+      section: 'Practice Documents',
+program: {
+name: 'Excel',
+image: 'https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/49aed2c6942f98e51c322cfcbe304f249faebc60/Excel%20Icon.png'
+},
+
+      title: 'Cleaning Excel Spreadsheets',
+     description: 'Learn how to clean, format, and organize messy spreadsheet data efficiently.',
+  thumbnail: 'https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/2b848924357fae50ad8b97a184b023721eb27c65/Thumbnails/Thumbnail%20Image%20Cleaning%20Spreadsheets.png',
+  hoverVideo: 'https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/2b848924357fae50ad8b97a184b023721eb27c65/Thumbnails/Excel%20-%20Cleaning%20Spreadsheets%20Thumbnail%20Video.mp4',
+
+      link: 'https://docs.google.com/spreadsheets/d/1nKJyR_6HYp-Spdruv048tgdeXU3_f3CB/export?format=xlsx'
     },
+
+
+
+
 
 {
   section: 'Practice Documents',
+program: {
+name: 'Excel',
+image: 'https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/49aed2c6942f98e51c322cfcbe304f249faebc60/Excel%20Icon.png'},
   title: 'Practice Workbook - Dynamic Text Filter',
   description: 'A downloadable spreadsheet to practice the dynamic text filter capability.',
   thumbnail: 'https://img.icons8.com/color/96/000000/microsoft-excel-2019--v1.png',
@@ -22,10 +36,19 @@ const contentItems = [
 
     {
       section: 'Videos',
-      title: 'Resume Tips',
-      description: 'Step-by-step resume writing strategies.',
-      thumbnail: 'https://img.icons8.com/ios/100/000000/video.png',
-      link: '#videos'
+program: {
+name: 'YouTube',
+image: 'https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/8c6a1dc0ea817ed635f08e464fa3b9cbbf6b7f93/Thumbnails/YouTube%20Thumbnail.png'
+},
+
+      title: 'Modern Resume Tips',
+      description: 'Step-by-step guide to resume writing strategies and a provided template.',
+      thumbnail: 'https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/8f7a31e22e2ef623ad6b605b1f8d06a0c71b24a3/Thumbnail%20Modern%20Resume%20Video.png',
+      link: 'https://youtu.be/0YgpqxPWmoc',
+related: {
+text: 'Modern resume template',
+url: 'https://docs.google.com/presentation/d/1q56RDcpNKYnM69j6ElT9LfV04faAcRDu/export?format=pptx'
+},
     },
 {
   section: 'Templates',
