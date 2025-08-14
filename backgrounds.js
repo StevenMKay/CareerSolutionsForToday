369e1a8479c98d3a64741ec8b4d97459a9a3e6c0
Steven Kay <steven.m.kay@live.com>
2025-08-13 18:01:44 -0700

    Update backgrounds.js

--- backgrounds.js
+++ backgrounds.js
@@ -4,7 +4,7 @@ const backgrounds = {
   "learn.html": "url('https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/8e0bcb89c429b415b157553ec88792eabb3099c9/photos/LearnSectionPhoto.jpg')",
   "videos.html": "url('https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/0786e5f3335fd538557bd2f731a5447697137168/photos/videosection2.png')",
   "templates.html": "url('https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/da19106f72f1e65b1251a02913a85dd58a98eff2/photos/ResumeSectionPhoto.jpg')",
-  "businessCase.html": "url('https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/d18c9151c29ed01f172dcecca8220e4a0a41fca6/businessCase.html')"
+  "businessCase.html": "url('https://raw.githubusercontent.com/StevenMKay/CareerSolutionsForToday/d18c9151c29ed01f172dcecca8220e4a0a41fca6/businessCase.html')",
 };
 
 // Get current page name and set background
@@ -13,3 +13,4 @@ if (backgrounds[page]) {
   document.body.style.backgroundImage = `linear-gradient(to right, rgba(0, 0, 0, 0.1) 20%, #7F7F7F 99%), ${backgrounds[page]}`;
 
 }
+
